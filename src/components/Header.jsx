fea1d54f4cfc0bcd278831b40495d5dc42e190b8
SushantSail <133793150+SushantSail@users.noreply.github.com>
2025-03-31 13:59:45 +0530

    Update /shoopingpage in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -41,7 +41,7 @@ export default function Header() {
               type="button"
               className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             >
-              <Link to="/aboutus">
+              <Link to="/shoopingpage">
               <img
               src="https://cdn.logojoy.com/app/editor/ic_cart.svg"
               className="h-5"
