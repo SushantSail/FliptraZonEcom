4b7052d12a97bb2cf199ba6f7a00eeec48500bfa
SushantSail <133793150+SushantSail@users.noreply.github.com>
2026-02-12 10:12:03 +0530

    Update Header.jsx
    change logo name

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -36,7 +36,7 @@ export default function Header() {
               alt="Cart_Logo"
             />
             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-              Fliptrazon
+            Shop-Nest
             </span>
           </NavLink>
           <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
